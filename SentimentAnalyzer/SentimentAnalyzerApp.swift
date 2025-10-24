0195e46a7e76072f3c0532eafca51954bb71c1a5
Sergey Petrosyan <ipetriez@icloud.com>
2025-10-24 18:09:59 +0400

    Initial Commit

---
+++ SentimentAnalyzer/SentimentAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentimentAnalyzerApp.swift
+//  SentimentAnalyzer
+//
+//  Created by Sergey Petrosyan on 24.10.25.
+//
+
+import SwiftUI
+
+@main
+struct SentimentAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
