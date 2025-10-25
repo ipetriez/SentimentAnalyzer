9b6621a1ebe1878ac457898d02e519c12451adf4
Sergey Petrosyan <ipetriez@icloud.com>
2025-10-25 12:39:48 +0400

    Build basic UI.

--- SentimentAnalyzer/ContentView.swift
+++ SentimentAnalyzer/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var responseText: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                ScrollView {
+                    Text("Chart")
+                    Text("Overview")
+                    
+                    ForEach(0 ..< 10) { response in
+                        Text("Response")
+                    }
+                }
+                HStack {
+                    TextField(
+                        "Your thoughts on the future of AI...",
+                        text: $responseText,
+                        axis: .vertical
+                    )
+                    .padding(12)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 25)
+                            .stroke(Color(.systemGray4), lineWidth: 1)
+                    }
+                    
+                    Button("Done") {
+                        
+                    }
+                    .fontWeight(.semibold)
+                }
+                .padding()
+            }
         }
-        .padding()
     }
 }
 
